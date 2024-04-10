0a4efbc54683298c044d4789defcfaf65d4ee83e
Sean Jilek <sjilek1@umbc.edu>
2024-04-10 11:01:53 -0400

    Reverse engineered the competitor

---
+++ Application/client/src/components/Login.js
@@ -0,0 +1,36 @@
+import React from "react";
+import AnimatedCard from "./motionComponents/AnimatedCard.js";
+import data from "../constants/index.js";
+
+function Login() {
+  const authUrl = `https://accounts.spotify.com/authorize?client_id=${data.CLIENT_ID}&response_type=code&redirect_uri=${data.REDIRECT_URI}&scope=${data.SCOPES}`;
+  return (
+    <>
+      <div className="min-h-screen flex flex-col items-center justify-around ">
+	<div className=" text-center">
+          <h1 className="p-4  text-2xl">Have You Heard This?</h1>
+          <h2>
+            Find small Artists based on your music tastes
+          </h2>
+        </div>
+        <a href={authUrl}>
+          <AnimatedCard className=" ">
+            <div className="mx-auto border rounded-md p-2 bg-green-400">
+              <div className="flex text-neutral-900  mx-2">
+                <img
+                  src="spotify-black.svg"
+                  width={25}
+                  alt="spotify"
+                  className="mx-2"
+                />
+                Log in with Spotify
+              </div>
+            </div>
+          </AnimatedCard>
+        </a>
+      </div>
+    </>
+  );
+}
+
+export default Login;
---
+++ Application/client/src/constants/index.js
@@ -0,0 +1,8 @@
+const data = {
+  CLIENT_ID: "ef99d0c9e56d4c889b3d1f6544e46b87",
+  REDIRECT_URI: "http://localhost:3000",
+  SCOPES:
+    "streaming%20user-read-private%20user-library-read%20user-library-modify%20playlist-modify-public",
+  AUTH_SERVER_URL: "http://localhost:3001/",
+};
+export default data;
