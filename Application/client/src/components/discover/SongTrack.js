c0f1ec8bf8b9c683c8e38da666a93d774a697193
LiamUMBC <wmackin1@umbc.edu>
2024-05-08 17:03:14 -0400

    song titles and artists

--- Application/client/src/components/discover/SongTrack.js
+++ Application/client/src/components/discover/SongTrack.js
@@ -64,28 +64,34 @@ function SongTrack({
   }, [audioUrl]);
 
   return (
-    <div
-      className="relative  hover:border-2 hover:border-sky-500"
-      onMouseOver={play}
-      onMouseOut={pause}
-    >
-      <img
-        whileTap={{ scale: "0.8" }}
-        className="absolute left-0 bottom-0 w-4 sm:w-8 p-1 cursor-pointer rotate-180"
-        src="aakash_unliked.png"
-        onClick={() => addDislikeHandler()}
-        alt=""
-      />
+    <div className = "flex flex-col items-center justify-between w-28 shadow-md rounded-lg shadow-indigo-950 bg-blue-950">
+      <div
+        className="relative  hover:border-2 hover:border-sky-500"
+        onMouseOver={play}
+        onMouseOut={pause}
+      >
+        <img src={imgUrl} alt="" />
+        <div className="flex justify-between">
+          <img
+            whileTap={{ scale: "0.8" }}
+            className="flex left-0 bottom-0 w-4 sm:w-8 p-1 cursor-pointer rotate-180"
+            src="aakash_unliked.png"
+            onClick={() => addDislikeHandler()}
+            alt=""
+          />
 
-      <motion.img
-        whileTap={{ scale: "0.8" }}
-        className="absolute bottom-0 right-0 w-4 sm:w-8 p-1 cursor-pointer"
-        src="like.svg"
-        onClick={() => likeSongHandler(id)}
-        alt=""
-      />
-
-      <img src={imgUrl} alt="" />
+          <motion.img
+            whileHover={{ scale: "1.275" }}
+            whileTap={{ scale: "0.8" }}
+            className="flex bottom-0 right-0 w-4 sm:w-8 p-1 cursor-pointer"
+            src="like.svg"
+            onClick={() => likeSongHandler(id)}
+            alt=""
+          />
+        </div>
+        <h1 className="text-slate-300 text-xs font-semibold">{song}</h1>
+        <h1 className="text-slate-300 text-xs font-semibold">by {artist}</h1>
+      </div>
     </div>
   );
 }
